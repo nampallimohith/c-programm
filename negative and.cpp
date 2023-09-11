19496cd616af9bbae8cc1ab89a3a6a55c3ada3b6
nampallimohith <128620062+nampallimohith@users.noreply.github.com>
2023-09-11 20:05:18 +0530

    Add files via upload

---
+++ negative and.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("the number is positive");
+	}
+	else if(n=0)
+	{
+		printf("number=0");
+		
+	}
+	else
+	{
+		printf(" the number is negative");
+	}
+	
+}
